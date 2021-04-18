dabbebe9dd75016a72e1c2541082118bba683c46
Federico Castelau <castelaufederico@gmail.com>
2021-04-18 15:04:00 -0300

    Imported SharedModule to PlanetsModule

--- src/app/planets/planets.module.ts
+++ src/app/planets/planets.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { PlanetsRoutingModule } from './planets-routing.module';
 import { PlanetsListComponent } from './planets-list/planets-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [PlanetsListComponent],
   imports: [
     CommonModule,
-    PlanetsRoutingModule
+    PlanetsRoutingModule,
+    SharedModule
   ]
 })
 export class PlanetsModule { }
