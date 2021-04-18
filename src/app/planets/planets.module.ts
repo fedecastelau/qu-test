463eaa8f51cc84d1ada60c0a96dd73b2c3072e1f
Federico Castelau <castelaufederico@gmail.com>
2021-04-18 15:14:38 -0300

    Created a service to Planets

--- src/app/planets/planets.module.ts
+++ src/app/planets/planets.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { PlanetsRoutingModule } from './planets-routing.module';
 import { PlanetsListComponent } from './planets-list/planets-list.component';
 import { SharedModule } from '../shared/shared.module';
+import { PlanetsService } from './planets.service';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { SharedModule } from '../shared/shared.module';
     CommonModule,
     PlanetsRoutingModule,
     SharedModule
-  ]
+  ],
+  providers: [PlanetsService]
 })
 export class PlanetsModule { }
