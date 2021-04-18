928b9971f6dfb4a4e9ab5f455629ae6a66758d68
Federico Castelau <castelaufederico@gmail.com>
2021-04-18 15:13:21 -0300

    Created route for planets, and added router-oulet into app.component

--- src/app/planets/planets-routing.module.ts
+++ src/app/planets/planets-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlanetsListComponent } from './planets-list/planets-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: PlanetsListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
