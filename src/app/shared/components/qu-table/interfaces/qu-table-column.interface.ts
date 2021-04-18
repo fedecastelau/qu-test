29873905d4dc4368ad3647e73837d1fa1ec986e8
Federico Castelau <castelaufederico@gmail.com>
2021-04-18 17:58:35 -0300

    Added interfaces, render list from the data and styles

---
+++ src/app/shared/components/qu-table/interfaces/qu-table-column.interface.ts
@@ -0,0 +1,7 @@
+export interface QuTableColumn extends Object {
+    /* It's the name of the key to get from results */
+    id: string;
+
+    /*  It's the string to put at the top of the table */
+    title: string;
+}
--- src/app/shared/components/qu-table/qu-table.component.ts
+++ src/app/shared/components/qu-table/qu-table.component.ts
@@ -1,11 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { SwapiList } from '../../interfaces/swapi.list.interface';
+import { QuTableColumn } from './interfaces/qu-table-column.interface';
 
 @Component({
-  selector: 'app-qu-table',
+  selector: 'qu-table',
   templateUrl: './qu-table.component.html',
   styleUrls: ['./qu-table.component.scss']
 })
 export class QuTableComponent implements OnInit {
+  @Input() columns: QuTableColumn[] = [];
+  @Input() data: SwapiList;
+
+  @Output() customEvent = new EventEmitter();
 
   constructor() { }
 
